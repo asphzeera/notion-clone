cea46fe9d2f21da5693879f4d93541ba32bbaf0d
asphzeera <asaphobrito@gmail.com>
2025-09-08 16:27:54 -0300

    Acabo de colocar um sistema de login no Zotion. Utilizando um aplicativo chamado Clerk. Ele é responsavel pela parte do login do WebSite. Utilizando ele, colocamos tal sistema de login, e adicionamos funcionalidades para logar,  etc. Proximo passo é criar uma pagina de Documentos

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,14 +54,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="zotion-theme-2">
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="zotion-theme-2">
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain :"https://fun-bluegill-92.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
